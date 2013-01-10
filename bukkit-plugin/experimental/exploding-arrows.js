fc7189f7342d8a1e9aa9a73bb1067fc9673e44e7
walterhiggins <walter.higgins@gmail.com>
2013-01-10 00:14:45 +0000

    removed unneeded comment

--- bukkit-plugin/experimental/exploding-arrows.js
+++ bukkit-plugin/experimental/exploding-arrows.js
@@ -2,9 +2,6 @@ importPackage(org.bukkit.entity);
 bukkit.on("entity.ProjectileHitEvent", function(listener, event){
     var projectile = event.entity;
     var world = projectile.world;
-    //
-    // only want arrows shot by players to explode
-    //
     if (projectile instanceof Arrow && projectile.shooter instanceof Player){
         projectile.remove();
         world.createExplosion(projectile.location,2.5);
