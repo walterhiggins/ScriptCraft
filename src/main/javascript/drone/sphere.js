4a586a115cc9eda1dc6a17c1fc7d54fde5e02a47
walterhiggins <walter.higgins@gmail.com>
2013-02-01 22:14:53 +0000

    added hemispheres and blocks ids

--- src/main/javascript/drone/sphere.js
+++ src/main/javascript/drone/sphere.js
@@ -56,3 +56,60 @@ Drone.extend('sphere0', function(block,radius)
         .sphere(0,radius-1)
         .back().left().down();
 });
+Drone.extend('hemisphere', function(block,radius, northSouth){
+    var lastRadius = radius;
+    var slices = [[radius,0]];
+    var diameter = radius*2;
+    var world = this._getWorld();
+    var bm = this._getBlockIdAndMeta(block);
+
+    var r2 = radius*radius;
+    for (var i = 0; i <= radius;i++){
+        var newRadius = Math.round(Math.sqrt(r2 - i*i));
+        if (newRadius == lastRadius)
+            slices[slices.length-1][1]++;
+        else
+            slices.push([newRadius,1]);
+        lastRadius = newRadius;
+    }
+    this.chkpt('hsphere');
+    //
+    // mid section
+    //
+    if (northSouth == "north"){
+        this.cylinder(block,radius,slices[0][1],{blockType: bm[0],meta: bm[1],world: world});
+    } else {
+        this.up(radius - slices[0][1])
+        .cylinder(block,radius,slices[0][1],{blockType: bm[0],meta: bm[1],world: world})
+        .down(radius - slices[0][1]);
+    }
+    
+    var yOffset = -1;
+    for (var i = 1; i < slices.length;i++)
+    {
+        yOffset += slices[i-1][1];
+        var sr = slices[i][0];
+        var sh = slices[i][1];
+        var v = yOffset, h = radius-sr;
+        if (northSouth == "north") {
+            // northern hemisphere
+            this.up(v).fwd(h).right(h)
+            .cylinder(block,sr,sh,{blockType: bm[0],meta: bm[1],world: world})
+            .left(h).back(h).down(v);
+        }else{
+            // southern hemisphere
+            v = radius - (yOffset+sh+1);
+            this.up(v).fwd(h).right(h)
+                .cylinder(block,sr,sh,{blockType: bm[0],meta: bm[1],world: world})
+                .left(h).back(h). down(v);
+        }
+    }
+    return this.move('hsphere');
+    
+});
+Drone.extend('hemisphere0', function(block,radius,northSouth){
+    return this.hemisphere(block,radius,northSouth)
+        .fwd().right().up(northSouth=="north"?0:1)
+        .hemisphere(0,radius-1,northSouth)
+        .back().left().down(northSouth=="north"?0:1);
+});
