6a73144c930c531c6d6b4727e4e21e0faea3bb0b
walterhiggins <walter.higgins@gmail.com>
2014-01-04 16:57:43 +0000

    generate table of contents for Young Persons guide

---
+++ src/docs/javascript/generateTOC.js
@@ -0,0 +1,34 @@
+args = args.slice(1);
+
+var template = args[0];
+
+var BufferedReader = java.io.BufferedReader;
+var FileReader = java.io.FileReader;
+
+var contents = [], line = undefined;
+var br = new BufferedReader( new FileReader(template) );
+
+while ( (line = br.readLine()) != null){
+    contents.push(line);
+}
+br.close();
+
+println('## Table of Contents');
+
+for (var i = 0; i < contents.length; i++){
+    line = contents[i];
+    if (line.match(/^##\s+/)){
+        var h2 = line.match(/^##\s+(.*)/)[1].trim();
+        var link = h2.replace(/[^a-zA-Z0-9 \-]/g,'');
+        link = link.replace(/ /g,'-');
+        link = link.toLowerCase();
+        println (' * [' + h2 + '](#' + link + ')');
+    }
+    if (line.match(/^###\s+/)){
+        var h3 = line.match(/^###\s+(.*)/)[1].trim();
+        var link = h3.replace(/[^a-zA-Z0-9 \-]/g,'');
+        var link = link.replace(/ /g,'-');
+        link = link.toLowerCase();
+        println ('   * [' + h3 + '](#' + link + ')');
+    }
+}
