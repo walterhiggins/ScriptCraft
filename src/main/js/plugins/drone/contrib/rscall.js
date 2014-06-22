d39c70c0fb17d0a97ef87ac579d7d422740c805c
Sam Kass <samkass@samkass.com>
2014-06-22 10:00:18 -0400

    Improved documentation.

--- src/main/js/plugins/drone/contrib/rscall.js
+++ src/main/js/plugins/drone/contrib/rscall.js
@@ -1,6 +1,21 @@
 var Drone = require('../drone').Drone;
 
-/**
+/************************************************************************
+ *
+ * Redstone events are world-wide, and sometimes you want an easy way to respond
+ * to just one button with something.  This drone extension does this!  To use,
+ * look at the button, switch, pressure plate, etc., that you would like to
+ * make do something.  Then, enter:
+ *
+ *   /js new Drone().rscall(function(event){ MY CODE HERE });
+ *
+ * For example,
+ *
+ *   /js new Drone().rscall(function(event){ console.log("My button was pressed!"); });
+ *
+ * You can also chain this with other Drone() commands in order to set up callbacks
+ * as part of a larger construction.
+ *
  * rscall(callback) will call the callback if a redstone event happens on a block at the location of the drone
  * rsremove will remove all callbacks added with rscall from the location of the drone
  *
