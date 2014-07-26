33e659013788560f91ca55f51b377fd2f0ef74e4
Sam Kass <samkass@samkass.com>
2014-07-26 10:18:05 -0400

    Update tabbing, use utils.locationToString, add some additional comments.

--- src/main/js/plugins/drone/contrib/rscall.js
+++ src/main/js/plugins/drone/contrib/rscall.js
@@ -20,29 +20,34 @@ var Drone = require('../drone').Drone;
  * rsremove will remove all callbacks added with rscall from the location of the drone
  *
  * (todo: should I be looking for block breaks at this location and removing callbacks?)
-*/
+ */
 
+// An object which stores the registered rscalls for later rsremove
 var rsRemoveFuncs = {};
 
-function locToStr(x,y,z) { return ""+x+","+y+","+z; }
+Drone.extend("rscall", function (callback) {
+    var newCallback = function (x, y, z) {
+        return function (event) {
+            if (event.block.location.blockX == x &&
+                event.block.location.blockY == y &&
+                event.block.location.blockZ == z) callback(event);
+        }
+    }(this.x, this.y, this.z);
 
-Drone.extend("rscall", function( callback ) {
-    var newCallback = function(x,y,z){
-                    return function(event) { if (event.block.location.blockX == x &&
-                                            event.block.location.blockY == y &&
-                                            event.block.location.blockZ == z) callback(event); }}(this.x,this.y,this.z);
-    var locStr = locToStr(this.x,this.y,this.z);
+    var locStr = utils.locationToString(new org.bukkit.Location(this.world, this.x, this.y, this.z));
     if (!(locStr in rsRemoveFuncs)) {
+        // First time for this location... create an empty array to store locations
         rsRemoveFuncs[locStr] = [];
     }
+    // Add callback to struct keyed by location string and stored in array
     rsRemoveFuncs[locStr].push(events.blockRedstone(newCallback));
 
-  return this;
+    return this;
 });
 
-Drone.extend("rsremove", function( ) {
-    var locStr = locToStr(this.x,this.y,this.z);
-    var removeList = rsRemoveFuncs[locToStr(this.x, this.y, this.z)];
+Drone.extend("rsremove", function () {
+    var locStr = utils.locationToString(new org.bukkit.Location(this.world, this.x, this.y, this.z));
+    var removeList = rsRemoveFuncs[locStr];
     if (removeList) {
         for (var i = 0; i < removeList.length; ++i) {
             removeList[i].unregister();
