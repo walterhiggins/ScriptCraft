7b6bed6fa17cda05815aaac70b05936013ef7626
Tony Gravagno <xh8r2nbu9q@snkmail.com>
2016-12-09 16:22:39 -0600

    generateTOC has preliminary disabled code for evaluation by Walter. Would like to know if it's worth pursuing. blocks.js has many significant updates. Note "breaking fix" of a "redston" item for "redstone". Existing apps will need to correct this if they use it.

--- src/docs/js/generateTOC.js
+++ src/docs/js/generateTOC.js
@@ -40,11 +40,34 @@ for (var i = 0; i < contents.length; i++){
     if (line.match(/^##\s+/)){
         var h2 = line.match(/^##\s+(.*)/)[1].trim();
         var link = createLink(h2);
-        java.lang.System.out.println (' * [' + h2 + '](#' + link + ')');
+        java.lang.System.out.println(' * [' + h2 + '](#' + link + ')');
     }
     if (line.match(/^###\s+/)){
         var h3 = line.match(/^###\s+(.*)/)[1].trim();
         var link = createLink(h3);
-        java.lang.System.out.println ('   * [' + h3 + '](#' + link + ')');
+        java.lang.System.out.println('   * [' + h3 + '](#' + link + ')');
     }
 }
+
+/**
+ * // tg 20161022 Sort secondary topics, with some exceptions
+ * // Still work in progress. Ignore
+ * var sortedContents =[];
+ * var sortedTemp = [];
+ * 
+ * for (var i = 0; i < contents.length; i++) {
+ *   line = contents[i];
+ *   if(line.match(/^##\s+/)) {
+ *     if (sortedTemp.length > 0) {
+ *       sortedContents.push(sortedTemp.sort());
+ *       sortedContents.push(line);
+ *     } else {
+ *       if (line.match(/^###\s+/)) {
+ *         sortedTemp.push(line);
+ *       } else {
+ *         sortedContents.push(line);
+ *       }
+ *     }
+ *   }
+ * contents = sortedContents;
+ */
