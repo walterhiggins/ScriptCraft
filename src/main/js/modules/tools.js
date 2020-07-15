6923b0242cb1f71f7ac172a4c3a31cf12039d240
hb432 <31751529+hb432@users.noreply.github.com>
2020-07-14 20:55:21 -0400

    Update tools.js

--- src/main/js/modules/tools.js
+++ src/main/js/modules/tools.js
@@ -1,8 +1,168 @@
+const FileInputStream = Java.type('java.io.FileInputStream');
+const FileOutputStream = Java.type('java.io.FileOutputStream');
+const Files = Java.type('java.nio.file.Files');
+const Paths = Java.type('java.nio.file.Paths');
+const Scanner = Java.type('java.util.Scanner');
+// ES modules are not supported on scriptcraft
+// const Source = Java.type('org.graalvm.polyglot.Source');
+const StandardCopyOption = Java.type('java.nio.file.StandardCopyOption');
+const URL = Java.type('java.net.URL');
+const ZipInputStream = Java.type('java.util.zip.ZipInputStream');
+
+/**
+ * @type {{
+ *    chain: core$chain,
+ *    fetch: core$fetch,
+ *    file: core$file,
+ *    unzip: core$unzip
+ * }}
+ */
 var core = {
+   /**
+    * @callback core$chain
+    * @param {any} base
+    * @param {function} modifier
+    * @returns {void}
+    */
    chain: (base, modifier) => {
       var chain = (object) => modifier(object, chain);
       chain(base);
    },
+   /**
+    * @callback core$fetch$json
+    * @returns {any}
+    */
+   /**
+    * @callback core$fetch$read
+    * @returns {string}
+    */
+   /**
+    * @callback core$fetch$stream
+    * @returns {any}
+    */
+   /**
+    * @callback core$fetch$unzip
+    * @param {any} to
+    * @returns {core$file$}
+    */
+   /**
+    * @callback core$fetch
+    * @param {string} from
+    * @returns {{
+    *    json: core$fetch$json,
+    *    read: core$fetch$read,
+    *    stream: core$fetch$stream,
+    *    unzip: core$fetch$unzip
+    * }}
+    */
+   fetch: (from) => {
+      var link = new URL(from).openConnection();
+      link.setDoOutput(true);
+      link.setRequestMethod('GET');
+      link.setInstanceFollowRedirects(true);
+      var code = link.getResponseCode();
+      if (code === 200) {
+         var thing = {
+            json: () => {
+               try {
+                  return JSON.parse(thing.read());
+               } catch (error) {
+                  return null;
+               }
+            },
+            read: () => {
+               return new Scanner(thing.stream()).useDelimiter('\\A').next();
+            },
+            stream: () => {
+               return link.getInputStream();
+            },
+            unzip: (to) => {
+               return core.unzip(thing.stream(), to);
+            }
+         };
+         return thing;
+      } else {
+         throw code;
+      }
+   },
+   /**
+    * @callback core$file$add
+    * @returns {core$file$}
+    */
+   /**
+    * @callback core$file$child
+    * @param {number} index
+    * @returns {core$file$}
+    */
+   /**
+    * @callback core$file$dir
+    * @returns {core$file$}
+    */
+   /**
+    * @callback core$file$file
+    * @param {...string} sub
+    * @returns {core$file$}
+    */
+   /**
+    * @callback core$file$flush
+    * @returns {core$file$}
+    */
+   /**
+    * @callback core$file$json
+    * @returns {any}
+    */
+   /**
+    * @callback core$file$parse
+    * @returns {core$file$}
+    */
+   /**
+    * @callback core$file$remove
+    * @returns {core$file$}
+    */
+   /**
+    * @callback core$file$stream
+    * @returns {any}
+    */
+   /**
+    * @callback core$file$transfer
+    * @param {any} to
+    * @param {string} action
+    * @returns {core$file$}
+    */
+   /**
+    * @callback core$file$write
+    * @param {string} content
+    * @returns {core$file$}
+    */
+   /**
+    * @callback core$file$unzip
+    * @param {any} to
+    * @returns {core$file$}
+    */
+   /**
+    * @typedef {{
+    *    add: core$file$add,
+    *    child: core$file$child,
+    *    dir: core$file$dir,
+    *    exists: boolean,
+    *    file: core$file$file,
+    *    flush: core$file$flush,
+    *    io: any,
+    *    json: core$file$json,
+    *    parse: core$file$parse,
+    *    read: core$file$read,
+    *    remove: core$file$remove,
+    *    stream: core$file$stream,
+    *    transfer: core$file$transfer,
+    *    write: core$file$write,
+    *    unzip: core$file$unzip
+    * }} core$file$
+    */
+   /**
+    * @callback core$file
+    * @param {...string} path
+    * @returns {core$file$}
+    */
    file: (...path) => {
       var io = Paths.get(...path).normalize().toFile();
       var thing = {
@@ -45,7 +205,7 @@ var core = {
                return null;
             }
          },
-         // ES6 modules are not supported on scriptcraft
+         // ES modules are not supported on scriptcraft
          /*
          parse: () => {
             io.exists() &&
@@ -91,6 +251,12 @@ var core = {
       };
       return thing;
    },
+   /**
+    * @callback core$unzip
+    * @param {any} from
+    * @param {any} to
+    * @returns {core$file$}
+    */
    unzip: (from, to) => {
       var stream = new ZipInputStream(from);
       try {
